b05830704878905c855d0e65c9e304a843fca071
Artwalk <frank.holy.art@gmail.com>
2014-10-21 19:39:00 +0800

    refactor: FeedbackVC

--- TimeHacker/FeedbackViewController.swift
+++ TimeHacker/FeedbackViewController.swift
@@ -17,10 +17,8 @@ class FeedbackViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var submitBarButtonItem: UIBarButtonItem!
     
-    var deviceInfo:NSDictionary?
-    
     override func viewDidLoad() {
-//        println(DeviceInfo().info())
+        
     }
     
     @IBAction func feedbackTextFieldEditingChanged(sender: UITextField) {
@@ -33,28 +31,41 @@ class FeedbackViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func submitBarButtonItemAction(sender: UIBarButtonItem) {
+        (submitBarButtonItem.enabled, emailTextField.enabled, feedbackTextField.enabled) = (false,false,false)
+        
         let url = NSURL(string:serverUrl)
         let cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData
-        var request = NSMutableURLRequest(URL: url, cachePolicy: cachePolicy, timeoutInterval: 3.0)
+        var request = NSMutableURLRequest(URL: url!, cachePolicy: cachePolicy, timeoutInterval: 3.0)
         request.HTTPMethod = "POST"
         
-        let jsonObject = ["data":DeviceInfo().info()]
+        let userInfoOjb = ["feedback":feedbackTextField.text, "email":emailTextField.text]
+        let jsonObject:Dictionary<String, AnyObject>! = ["userInfo":userInfoOjb ,"deviceInfo":DeviceInfo().info()]
         
         if NSJSONSerialization.isValidJSONObject(jsonObject) {
-            if let data = NSJSONSerialization.dataWithJSONObject(jsonObject, options: NSJSONWritingOptions.PrettyPrinted, error: nil) {
-                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-                request.HTTPBody = jsonStr.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+            if let data = NSJSONSerialization.dataWithJSONObject(jsonObject, options:nil, error: nil) {
+                
+                let string = NSString(data: data, encoding: NSUTF8StringEncoding)
+                request.HTTPBody = ("data=" + string!).dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
             }
         }
         
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
-//            println(error)
-//            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+            var msg:String!
+            if error != nil {
+                println(error)
+                msg = "Sorry, Submit failed!"
+                self.submitBarButtonItem.enabled = true
+            } else {
+                msg = "Your feedback has been submitted."
+                (self.emailTextField.text, self.feedbackTextField.text) = ("" ,"")
+                self.submitBarButtonItem.enabled = false
+            }
+            
+            (self.emailTextField.enabled, self.feedbackTextField.enabled) = (true,true)
+            let alert = UIAlertController(title: nil, message: msg, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
         }
-        
-        
         
     }
 }
