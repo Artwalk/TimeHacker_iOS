74aa71e405df7fa5c04a18eb7833dd9b866393ac
Artwalk <frank.holy.art@gmail.com>
2015-01-02 05:29:07 +0800

    1. TabBar; 2. AppDelegate.m -> .swfit

--- TimeHacker/TimeHacker-Bridging-Header.h
+++ TimeHacker/TimeHacker-Bridging-Header.h
@@ -3,3 +3,4 @@
 //
 
 #import "THPomo.h"
+#import "THTodoListViewController.h"
--- TimeHacker/main.m
+++ TimeHacker/main.m
@@ -8,11 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import "THAppDelegate.h"
+#import "TimeHacker-Swift.h"
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
